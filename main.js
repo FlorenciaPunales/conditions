a1a854ef2c0b3db68c67897dcb783f4ac8fbf377
florenciapunales <fpp2027@gmail.com>
2023-01-25 12:26:01 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,83 @@
+// let colorBandera1 = prompt("Ingrese color de bandera")
+
+// if (colorBandera1 === "verde") {
+//     console.log("Me meto al agua")
+// }
+// else if (colorBandera1 === "amarilla") {
+//     console.log("Me meto al agua con cuidado")
+// }
+// else {
+//     console.log("no me meto al agua")
+// }
+
+// let edad = prompt("ingresa tu edad")
+
+// if (edad === "18") {
+//     alert("Sos mayor de edad")
+// }
+// else if (edad > 18) {
+//     alert("Eres mayor de 18")
+// }
+// else {
+//     alert("Menor de edad")
+// }
+
+// let edad = prompt("Ingresa tu edad")
+// let conMayor = prompt("En caso de que usted sea menor, viene con un mayor?")
+
+// if ((edad >= 18) || (conMayor == "si")) {
+//     console.log("Podes ingresar")
+// }
+// else {
+//     console.log("No podes pasar porque no sos mayor ni venis con uno")
+// }
+// let edad = 18
+
+// console.log( edad === "18")
+// let edad = prompt("Ingresa edad")
+// if ((edad > 0) && (edad < 100)) {
+//     console.log("Tenes entre 1 y 100")
+// }
+// else {
+//     console.log("ingrese numero valido")
+// }
+
+// let compraUno = prompt("Ingresa tu primer compra")
+// let compraDos = prompt("Ingresa tu segunda compra")
+
+// if ((compraUno == "pan") && (compraDos == "leche")) {
+//     console.log("Compraste ambos ingredientes")
+// }
+// else if ((compraUno == "pan") || (compraDos == "leche")) {
+//     console.log("Compraste solo un ingrediente")
+// }
+// else {
+//     console.log("No compraste nada")
+// }
+
+// && 
+// true && true = true
+// true && false = false
+// false && false = false
+
+// || 
+// true || true = true
+// true || false = true
+// false || false = false
+
+// let idioma = prompt("Ingresa tu idioma")
+
+// switch (idioma) {
+//     case "Español":
+//         console.log("Bienvenido");
+//         break;
+//     case "Japonés":
+//         console.log("Okaeri");
+//         break;
+//     case "Italiano":
+//         console.log("Ben Arrivato")
+//         break;
+    
+//     default: 
+//         console.log("Welcome")
+// }
